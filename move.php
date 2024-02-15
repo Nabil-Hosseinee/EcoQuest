fd8afdc6b13011af17136f347c54bb579d3f8833
DylanDumont <dyl87.dumont@gmail.com>
2024-02-15 10:45:33 +0100

    Changements sur move.php / move.php opérationnel

--- move.php
+++ move.php
@@ -3,21 +3,21 @@ session_start();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-$identifiant = $_POST["Identifiant"];
-$_POST["Identifiant"] = filter_input(INPUT_POST,"Identifiant", FILTER_SANITIZE_SPECIAL_CHARS);
-$mdp = sha1($_POST["Mot de passe"]); 
+$identifiant = $_POST["id"];
+$_POST["id"] = filter_input(INPUT_POST,"id", FILTER_SANITIZE_SPECIAL_CHARS);
+$mdp = sha1($_POST["mdp"]); 
 
 include('connect_bdd.php');
 
-$userStatement = $db->prepare("SELECT * FROM ecotracker");
+$userStatement = $db->prepare("SELECT * FROM user");
 $userStatement ->execute();
 $user = $userStatement->fetchAll();
 
 foreach ($user as $users) {
-    ($identifiant == $users['Identifiant'] && $mdp == $users['Mot de passe'])
+    ($identifiant == $users['Identifiant'] && $mdp == $users['Mot de passe']);
     $_SESSION["authentification"]= "OK";
     $_SESSION['Identifiant']= $identifiant;
     $_SESSION['Mot de passe'] = $mdp;
-    header("Location: accueil.php");    
+    header("Location: accueil.php");
 }
 ?>
