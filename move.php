adcd6cc1a160c55a5caaf6cdcd3537fa37e1991a
DylanDumont <dyl87.dumont@gmail.com>
2024-02-15 09:26:24 +0100

    Code pour se connecter sur move.php

--- move.php
+++ move.php
@@ -1,5 +1,23 @@
-<?php
+<?php 
+session_start();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
-echo 'hello';
+$identifiant = $_POST["Identifiant"];
+$_POST["Identifiant"] = filter_input(INPUT_POST,"Identifiant", FILTER_SANITIZE_SPECIAL_CHARS);
+$mdp = sha1($_POST["Mot de passe"]); 
 
+include('connect_bdd.php');
+
+$userStatement = $db->prepare("SELECT * FROM ecotracker");
+$userStatement ->execute();
+$user = $userStatement->fetchAll();
+
+foreach ($user as $users) {
+    ($identifiant == $users['Identifiant'] && $mdp == $users['Mot de passe'])
+    $_SESSION["authentification"]= "OK";
+    $_SESSION['Identifiant']= $identifiant;
+    $_SESSION['Mot de passe'] = $mdp;
+    header("Location: accueil.php");    
+}
 ?>
