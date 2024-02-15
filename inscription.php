0c4fd709776afc3af3736119bf484091d737c913
DylanDumont <dyl87.dumont@gmail.com>
2024-02-15 17:45:30 +0100

    Inscription va bien dans la bdd et redirige dans l'accueil

--- inscription.php
+++ inscription.php
@@ -1,19 +1,24 @@
 <?php
-
 include("connect_bdd.php");
 
-if (isset($_POST['pseudo']) && isset($_POST['mdp'])) {
+if (isset($_POST['pseudo'], $_POST['mdp'], $_POST['id'])) {
     $pseudo = $_POST['pseudo'];
     $mdp = sha1($_POST['mdp']);
     $id = $_POST['id'];
 
-    $sql = "INSERT INTO user (Identifiant, Pseudo, Mot de passe) VALUES ($id, $pseudo, $mdp)";
-    $result = $db->prepare($sql);
-    $result->execute();
+    $sql = "INSERT INTO user (Identifiant, Pseudo, `Mot de passe`) VALUES (:id, :pseudo, :mdp)";
+    $stmt = $db->prepare($sql);
 
-    if ($result) {
+    // Liaison des valeurs des paramètres
+    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':pseudo', $pseudo);
+    $stmt->bindParam(':mdp', $mdp);
+
+    // Exécution de la requête
+    if ($stmt->execute()) {
         echo 'Inscription réussie!';
         header('Location: accueil.php');
+        exit(); // Assurer qu'aucun code supplémentaire n'est exécuté après la redirection
     } else {
         echo 'Erreur lors de l\'inscription.';
     }
