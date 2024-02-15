cc57b1376851b8a804d55b192d38902063abe32f
Nabil <nabil.hosseinee@gmail.com>
2024-02-15 19:32:52 +0100

    résolution bug connexion

--- move.php
+++ move.php
@@ -1,9 +1,14 @@
 <?php 
 session_start();
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 $identifiant = $_POST["id"];
 $_POST["id"] = filter_input(INPUT_POST,"id", FILTER_SANITIZE_SPECIAL_CHARS);
 $mdp = sha1($_POST["mdp"]);
+
+echo $identifiant;
+echo $mdp;
 
 include('connect_bdd.php');
 
@@ -11,24 +16,13 @@ $userStatement = $db->prepare("SELECT * FROM user");
 $userStatement ->execute();
 $user = $userStatement->fetchAll();
 
-$found = false;
-
 foreach ($user as $users) {
-    if ($identifiant == $users['Identifiant'] || $identifiant == $users['Pseudo'] && $mdp == $users['Mot de passe']) {
+    if (($identifiant == $users['Identifiant'] || $identifiant == $users['Pseudo']) && $mdp == $users['Mot de passe']) {
         header("Location: accueil.php");
     }
     else {
         $message = "Identifiant ou mot de passe invalide.";
         echo "<script>alert('$message'); window.location.href = 'index.html';</script>";
     }
-}
-
-if ($found) {
-    header("Location: accueil.php");
-    exit; // Assurez-vous de quitter le script après une redirection
-} else {
-    $_SESSION["error_message"] = $message;
-    header("Location: index.php");
-    exit; // Assurez-vous de quitter le script après une redirection
 }
 ?>
