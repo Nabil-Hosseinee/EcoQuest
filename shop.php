5f09053172a3c0340bf4fa4dec0ce9b718adb920
Guenier Mathieu <90518115+Freezeblase@users.noreply.github.com>
2024-03-19 15:07:41 +0100

    shop

--- shop.php
+++ shop.php
@@ -12,21 +12,47 @@ $identifiant = $_SESSION['identifiant'];
 // Exécution de la requête SQL
 $requete = $db->prepare(
     "SELECT * FROM item
-    LEFT JOIN  Obtenu ON acquisition = 1
-    LEFT JOIN item_id 
-    WHERE Pseudo = '$identifiant'");
+    LEFT JOIN  acquisition ON item.id_item = acquisition.item_Id 
+    LEFT JOIN user ON user.id_user = acquisition.user_Id 
+    WHERE user.Pseudo = '$identifiant' AND acquisition.Obtenu = 1"); 
 $requete->execute();
-
-SELECT * FROM objets
-LEFT JOIN objets_utilisateurs ON objets.id = objets_utilisateurs.id_objet AND objets_utilisateurs.id_utilisateur = 1
-WHERE objets_utilisateurs.id_objet IS NULL;
-
 
 
 // Récupération des résultats
 $resultatsitem = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-$ownitem: $resultatsitem;
+$ownitem= $resultatsitem;
+
+
+if ($ownitem) {
+
+    if ($ownitem == $prix) {
+        // L'utilisateur a assez d'argent pour acheter l'objet
+        $nouvelle_money = $money - $prix;
+        // Mettre à jour la money dans la base de données
+        $mise_a_jour = $connexion->prepare("UPDATE user SET Monnaie = :nouvelle_monnaie WHERE Pseudo = '$identifiant'");
+        $mise_a_jour->bindParam(':nouvelle_monnaie', $nouvelle_money);
+        $mise_a_jour->execute();
+
+        echo '<script>alert("Félicitations ! Vous avez acheté  l\'objet, Votre nouveau solde est : " + $nouvelle_money + ".");</script>';
+    } else {
+        // L'utilisateur n'a pas assez d'argent pour acheter l'objet
+        echo '<script>alert("Désolé, vous n\'avez pas assez d\'argent pour acheter l\'objet, allez faire plus de défi.");</script>';
+    }
+} else {
+    echo "Utilisateur non trouvé.";
+}
+
+
+
+
+
+
+
+
+
+
+
 
 // Exécution de la requête SQL
 $requete = $db->prepare("SELECT Monnaie, Pseudo FROM user WHERE Pseudo = '$identifiant'");
@@ -37,7 +63,7 @@ $resultatsmoney = $requete->fetchAll(PDO::FETCH_ASSOC);
 
 $money: $resultatsmoney;
 
-if ($resultat) {
+if ($money) {
 
     if ($money >= $prix) {
         // L'utilisateur a assez d'argent pour acheter l'objet
