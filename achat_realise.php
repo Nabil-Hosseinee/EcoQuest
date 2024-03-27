a520ed5495b8ab17ac50112577b2528edba5d256
Nabil <nabil.hosseinee@gmail.com>
2024-03-27 14:02:45 +0100

    correction back shop

--- achat_realise.php
+++ achat_realise.php
@@ -10,11 +10,10 @@ if (isset($_POST['itemId'])) {
     if (isset($_SESSION['id_number'])) {
         $user_id = $_SESSION['id_number'];
 
-        $sql_insert_acquisition = "INSERT INTO acquisition (user_Id, item_Id, `Date achat`, Obtenu) 
-        VALUES (:user_id, :item_id, :date_achat, 1)";
+        $sql_insert_acquisition = "INSERT INTO acquisition (user_Id, item_Id, `Date achat`, Obtenu) VALUES (:user_id, :item_id, :date_achat, 1)";
         $result_insert_acquisition = $db->prepare($sql_insert_acquisition);
         $result_insert_acquisition->bindParam(':user_id', $user_id);
-        $result_insert_acquisition->bindParam('item_id', $id_item);
+        $result_insert_acquisition->bindParam(':item_id', $id_item);
         $result_insert_acquisition->bindParam(':date_achat', $date);
 
         echo "Update fait";
